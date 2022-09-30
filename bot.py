4e2213aae7da82f0856aee0ee2287e045e2478a1
WarSmile <w@vvnet.ru>
2022-09-30 13:50:37 +0300

    Add update3 Telegramm BOT

--- bot.py
+++ bot.py
@@ -1,5 +1,14 @@
-from telegram.ext import Updater, CommandHandler, CallbackContext
+import logging
+import settings
+# Подгружаем из репозитория ФУНКЦИИ (точка - разделитель папок, ипортируются используемые в проекте функции)
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+					level=logging.INFO,
+					filename='bot.log'
+					)
+
+# Определяем команды для старта
 def start_bot(update:Updater, context: CallbackContext):
 	print(update)
 	mytext = """Hello {}
@@ -7,13 +16,21 @@ def start_bot(update:Updater, context: CallbackContext):
 	I have only /start comand: ;)""".format(update.message.chat.first_name)
 	update.message.reply_text(mytext)
 
+def chat(update:Updater, context: CallbackContext):
+	text = update.message.text
+	logging.info(text)
+
+	update.message.reply_text(text)
+
 def main():
-	updtr = Updater ("296557628:AAEE_tE9q2xCh5kWhc8unKBEwkA6JEj_9K8")
+	updtr = Updater (settings.TOKEN_TELEGRAM)
 
 	updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+	updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
 
 	updtr.start_polling()
 	updtr.idle()
 
 if __name__ == "__main__":
+	logging.info('Bot started!')
 	main()
